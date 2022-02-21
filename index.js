dd3887cca77c35e841b26400510a94e088f87a56
Restyled.io <commits@restyled.io>
2022-02-21 19:40:33 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import "react-native-url-polyfill/auto";
 import "react-native-get-random-values";
 
 import messaging from "@react-native-firebase/messaging";
-import {AppRegistry} from "react-native";
+import { AppRegistry } from "react-native";
 
-import {name as appName} from "./app.json";
+import { name as appName } from "./app.json";
 import App from "./src/app/App";
 
 messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-  console.log("Message handled in the background!", remoteMessage);
+	console.log("Message handled in the background!", remoteMessage);
 });
 
 AppRegistry.registerComponent(appName, () => App);
